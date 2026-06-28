ef84133da3592d8977fbd62ce159a6ada5abf734
rosenii <2273657807mm@gmail.com>
2026-06-28 08:05:56 +0800

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -1087,6 +1087,7 @@ function getHtml() {
             font-size: 0.5rem;
             font-weight: 600;
             margin-left: 3px;
+            margin-bottom: 1px;
             background: rgba(255,255,255,0.15);
             vertical-align: middle;
         }
